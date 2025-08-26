2b77604b47b1f03ee61bf424b41cfc0bc2777f8f
JustLegits <dol23239@gmail.com>
2025-08-26 11:00:00 +0700

    Update local_rejoin.py

--- local_rejoin.py
+++ local_rejoin.py
@@ -198,7 +198,7 @@ def auto_rejoin():
                     launch_roblox(pkg, link)
                     send_webhook(f"{username} offline → rejoined {pkg}")
                 time.sleep(3)
-            time.sleep(60)
+            time.sleep(200)
     except KeyboardInterrupt:
         msg("[i] Dừng auto rejoin.")
 
