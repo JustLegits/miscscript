18ce2fdbb8fb8f143fe673aaa35823606cd181c6
JustLegits <dol23239@gmail.com>
2025-11-26 21:39:34 +0700

    add anti afk

--- AnimeStoryWebhook.lua
+++ AnimeStoryWebhook.lua
@@ -5,6 +5,13 @@
 --   2) Player Level (leaderstats.Level)
 --   3) Total Gems (LocalPlayer.Data.Gems)
 --   4) Total Coins (LocalPlayer.Data.Coins)
+--anti afk kick
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
 
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
