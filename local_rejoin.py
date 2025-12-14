588d7375e72df9b100c6b24e64f2cf211fb15534
JustLegits <dol23239@gmail.com>
2025-12-14 16:32:29 +0700

    Update local_rejoin.py

--- local_rejoin.py
+++ local_rejoin.py
@@ -69,7 +69,7 @@ def save_server_links(links):
 # ============ Roblox actions ============
 def get_custom_packages():
     # Các từ khóa nhận diện package
-    keywords = ["roblox", "bduy", "mangcut", "concacug","codex","delta","arceus","ugpornkiki"]
+    keywords = ["roblox", "bduy", "mangcut", "concacug","codex","delta","arceus","ugpornkiki","nhat"]
 
     result = subprocess.run(
         "pm list packages",
